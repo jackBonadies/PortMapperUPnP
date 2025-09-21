3782718f4fbc4e7848b83013f47e18517ad953c4
jackBonadies <jbonadies6@gmail.com>
2025-09-21 13:50:56 -0400

    update renew within expiring value to 20 seconds

--- app/src/main/java/com/shinjiindustrial/portmapper/PortForwardApplication.kt
+++ app/src/main/java/com/shinjiindustrial/portmapper/PortForwardApplication.kt
@@ -1,18 +1,8 @@
 package com.shinjiindustrial.portmapper
 
 import android.app.Application
-import android.content.Context
-import android.widget.Toast
-import androidx.activity.ComponentActivity
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.unit.dp
-import com.shinjiindustrial.portmapper.domain.PortMappingWithPref
 import dagger.hilt.android.HiltAndroidApp
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import java.util.logging.Logger
 import javax.inject.Inject
 
 
@@ -29,15 +19,14 @@ class PortForwardApplication : Application() {
     companion object {
 
         //        lateinit var showPopup : MutableState<Boolean>
-        var PaddingBetweenCreateNewRuleRows = 4.dp
+        val PaddingBetweenCreateNewRuleRows = 4.dp
 
         // can create a singleton logs repo
 
-        val ScrollToBottom = "ScrollToBottom"
-        var crashlyticsEnabled: Boolean = false
+        const val ScrollToBottom = "ScrollToBottom"
 
-        val RENEW_RULE_WITHIN_X_SECONDS_OF_EXPIRING = 30L
-        val RENEW_BATCH_WITHIN_X_SECONDS = 5L
+        const val RENEW_RULE_WITHIN_X_SECONDS_OF_EXPIRING = 20L
+        const val RENEW_BATCH_WITHIN_X_SECONDS = 5L
     }
 
 
